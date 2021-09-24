cc803f49cb3f398a17c585db6ab431a6ffdbbe24
Connor Groizard <ceg0026@mix.wvu.edu>
2021-09-24 16:29:24 -0400

    initial changes

---
+++ src/app/footer/foot-bar.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component ({
+selector: 'app-foot-bar',
+templateUrl: './foot-bar.component.html'})
+export class FootBarComponent {}
---
+++ src/app/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  templateUrl: './hello.component.html',
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
+})
+export class HelloComponent {
+  @Input() name!: string;
+}
